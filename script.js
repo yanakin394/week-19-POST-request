4d335746ffb11f8c5ab9b53217485ea4a7b5202a
yanakin394 <yana.germatskaya@gmail.com>
2023-06-04 21:32:50 +0300

    correction

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+//получаю необходимые элементы
 let postTitle = document.querySelector('.post__title');
 let postText = document.querySelector('.post__text');
 const postContainer = document.querySelector('.post__container');
 const btn = document.querySelector('.post__button');
 
-let postWrapper = document.createElement('div');
-postWrapper.classList.add('post__wrapper');
-
+//создаю функцию с фетч запросом
 function post () {
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         body: JSON.stringify({
-            title: postTitle,
-            body: postText,
+            title: postTitle.value,
+            body: postText.value,
         }),
         headers: {
             "Content-type": "application/json; charset=UTF-8",
@@ -19,6 +18,8 @@ function post () {
     })
     .then(response => response.json())
     .then((json) => {
+        let postWrapper = document.createElement('div');
+        postWrapper.classList.add('post__wrapper');
         console.log(json.title);
         postWrapper.innerHTML = `
             <p class="posted__title">${json.title}</p>
@@ -26,11 +27,15 @@ function post () {
     `;
     postContainer.appendChild(postWrapper);    
     })
+    .catch((err) => {
+        console.log('Ошибка!' + err);
+    })
 }
 
+//добавляю обработчик событий для кнопки, для добавления поста
 btn.addEventListener ('click', event => {
     event.preventDefault();
-    postTitle = document.querySelector('.post__title').value;
-    postText = document.querySelector('.post__text').value;
+    postTitle = document.querySelector('.post__title');
+    postText = document.querySelector('.post__text');
     post();
 });
